93f2f4521b4e6ab9a130b36cebac8b14b93cf67e
Ahad0712 <85945961+Ahad0712@users.noreply.github.com>
2021-10-26 15:31:36 +0530

    Update Chain.js

--- Chain.js
+++ Chain.js
@@ -1,21 +1,28 @@
 class Chain{
-    constructor(bodyA,pointB){
+    constructor(body1,body2,offsetX,offsetY){
+        this.offsetX=offsetX
+        this.offsetY=offsetY
         var options = {
-            bodyA:bodyA,
-            pointB:pointB,
-            stiffness:0.04,
-            length:200
+            bodyA : body1,
+            bodyB : body2,
+            pointB : {x:this.offsetX,y:this.offsetY}
         }
-        this.pointB=pointB
-        this.chain=Constraint.create(options);
-        World.add(world ,this.chain);
+        this.rope=Constraint.create(option)
+        World.add(world,this.rope)
     }
     display(){
-        if (this.chain.bodyA) {
-            var pointA = this.bodyA.position;
-            var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x,pointA.y,pointB.x,pointB.y);
-        }
+        var pointA=this.rope.bodyA.position;
+        var pointB=this.rope.bodyB.position;
+        
+        strokeWeight(2);
+        
+        var Anchor1X=pointA.x
+        var Anchor1Y=pointA.y
+        
+        var Anchor2X=pointB.x+this.offsetX
+        var Anchor2Y=pointB.y+this.offsetY
+        
+        line(Anchor1X,Anchor1Y,Anchor2X,Anchor2Y);
+    }
     }
 }
